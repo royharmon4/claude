139bb15ca756bbc06f0320fa3a120c2653b48fd5
Roy Harmon <royharmon4@gmail.com>
2026-06-07 16:05:36 -0400

    Add modular game registry

---
+++ src/games/index.js
@@ -0,0 +1,27 @@
+import TapRace from "./TapRace"
+import ReactionDuel from "./ReactionDuel"
+import FakeoutTap from "./FakeoutTap"
+import HigherLower from "./HigherLower"
+import SneakyMemory from "./SneakyMemory"
+import BalanceMeter from "./BalanceMeter"
+import Rps from "./Rps"
+import StopTheBar from "./StopTheBar"
+import ColorTrap from "./ColorTrap"
+import HoldRelease from "./HoldRelease"
+import PatternGame from "./PatternGame"
+import StopwatchGame from "./StopwatchGame"
+
+export const GAME_COMPONENTS = {
+  "tap-race": TapRace,
+  "reaction-duel": ReactionDuel,
+  "fakeout-tap": FakeoutTap,
+  "higher-lower": HigherLower,
+  "sneaky-memory": SneakyMemory,
+  "balance-meter": BalanceMeter,
+  "rock-paper-scissors": Rps,
+  "stop-the-bar": StopTheBar,
+  "color-trap": ColorTrap,
+  "hold-release": HoldRelease,
+  "memory-pattern": PatternGame,
+  "estimate-time": StopwatchGame,
+}
