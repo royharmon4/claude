03f293264e40e03488dedaa14d8ae2fe808b8b77
Roy Harmon <royharmon4@gmail.com>
2026-06-07 14:45:41 -0400

    Add point screen component

---
+++ src/components/PointScreen.jsx
@@ -0,0 +1,12 @@
+export default function PointScreen({ players, pointResult, onNextPoint }) {
+  const { pointWinnerIdx, seriesWins, gameName } = pointResult
+  return (
+    <div className="point-screen">
+      <div className="ps-label">POINT WINNER</div>
+      <div className={`ps-name ${pointWinnerIdx === 0 ? "t-pink" : "t-cyan"}`}>{players[pointWinnerIdx].name}</div>
+      <div className="ps-score">{seriesWins[0]}-{seriesWins[1]}</div>
+      <div className="series-note">{gameName} is best 2 out of 3. First to 2 gives the other player a strike.</div>
+      <button className="btn btn-cyan" onClick={onNextPoint} style={{ marginTop: 12 }}>NEXT POINT →</button>
+    </div>
+  )
+}
