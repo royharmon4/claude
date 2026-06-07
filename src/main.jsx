e22844e11fc415465ecb712c12426954bde6a027
Roy Harmon <royharmon4@gmail.com>
2026-06-07 16:12:50 -0400

    Load modular app

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
-import AppBalanceMeter from "./AppBalanceMeter.jsx"
+import App from "./App.jsx"
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppBalanceMeter />
+    <App />
   </React.StrictMode>
 )
