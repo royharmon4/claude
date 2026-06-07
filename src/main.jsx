a4bbaf8533aac1340993a3cfbd6c3bb57e6268f8
Roy Harmon <royharmon4@gmail.com>
2026-06-07 14:03:24 -0400

    Use app with Balance Meter

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
-import AppSneakyMemory from "./AppSneakyMemory.jsx"
+import AppBalanceMeter from "./AppBalanceMeter.jsx"
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppSneakyMemory />
+    <AppBalanceMeter />
   </React.StrictMode>
 )
