b627db655ee4be966d2c916917b8334d0f60f101
Roy Harmon <royharmon4@gmail.com>
2026-06-07 17:54:31 -0400

    Add history limit constant

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,6 @@
 export const STRIKES_WIN = 3
 export const SERIES_WIN = 2
 export const TARGET_MS = 5000
+export const HISTORY_LIMIT = 10
 export const LS_NAMES = "strike-zone:names"
 export const LS_HIST = "strike-zone:history"
