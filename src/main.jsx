f2005a214fabc14cb25be11346abcae161f5f59c
Roy Harmon <royharmon4@gmail.com>
2026-06-07 13:57:10 -0400

    Use app with Sneaky Memory

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react"
 import { createRoot } from "react-dom/client"
-import AppDeckHigherLower from "./AppDeckHigherLower.jsx"
+import AppSneakyMemory from "./AppSneakyMemory.jsx"
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppDeckHigherLower />
+    <AppSneakyMemory />
   </React.StrictMode>
 )
